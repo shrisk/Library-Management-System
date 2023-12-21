ae1b272ce11f97f55a0ff83f4fe3d3ed9d955a7a
K Srikanth <k.srikanth@mphasis.com>
2023-12-21 10:35:38 +0530

    revert

--- src/main/java/com/lms/LibraryApplication.java
+++ src/main/java/com/lms/LibraryApplication.java
@@ -2,10 +2,9 @@ package com.lms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LibraryApplication extends SpringBootServletInitializer {
+public class LibraryApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LibraryApplication.class, args);
