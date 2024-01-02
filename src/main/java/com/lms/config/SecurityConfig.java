79772bdc68578598e528bc3b2e2e16a585036c50
K Srikanth <k.srikanth@mphasis.com>
2024-01-02 11:51:04 +0530

    sprig security

--- src/main/java/com/lms/config/SecurityConfig.java
+++ src/main/java/com/lms/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/library/visualization/barChart").authenticated()
+                .antMatchers("/visualization/**").authenticated()
                 .anyRequest().permitAll()
                 .and()
             .httpBasic()
